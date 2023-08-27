de0da242b94e5a9d23797875ed34df0c90f1f1d5
moemaair <rageali12@gmail.com>
2023-08-27 21:39:15 +0300

    Simple hello and world as an api

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,24 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
 @SpringBootApplication
 public class DemoApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
+	}
+
+	@GetMapping
+	public static List<String> getD(String[] args) {
+		return List.of("Hello", "world");
 	}
 
 }
